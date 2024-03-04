81f2c8c3b03320a14f24d73ab69ab1d9e0cef2fb
Angel Gonzalez <angel.gonzalez6185@alumnos.udg.mx>
2024-03-04 16:51:44 -0600

    def

---
+++ proyectos/def/2definir.py
@@ -0,0 +1,33 @@
+
+
+def suma(numero1,numero2):
+    print(numero1+numero2)
+    print("Estoy sumando en una funcion")
+
+
+def multiplicacion(numero1,numero2):
+    print(numero1*numero2)
+    print("Estoy multiplicando en una funcion")
+
+
+def multiplicacion3(numero1,numero2,numero3):
+    print(numero1*numero2*numero3)
+    print("Estoy multiplicando 3 numeros en una funcion")
+
+def division(numero1,numero2):
+    print(numero1/numero2)
+    print("Estoy dividiendo en una funcion")
+
+suma(2,2)
+
+suma(10,20)
+
+multiplicacion(2,2)
+
+multiplicacion(10,20)
+
+multiplicacion3(3,4,5)
+
+multiplicacion3(5,6,7)
+
+division(6,7)
---
+++ proyectos/def/3definir.py
@@ -0,0 +1,18 @@
+def suma(numero1,numero2):
+    total= numero1 + numero2
+    print(total)
+    print("Estoy sumando en una funcion")
+    return total
+
+
+def multiplicacion(numero1,numero2):
+    total = numero1 * numero2
+    print(total)
+    print("Estoy multiplicando en una funcion")
+    return total
+
+
+
+suma(2,2)
+
+multiplicacion(3,4)
