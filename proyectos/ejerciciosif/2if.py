58878384bd8d9cf208a1e7540f2ed6a0b674d6db
Angel Gonzalez <angel.gonzalez6185@alumnos.udg.mx>
2024-03-20 17:27:59 -0600

    mejoras

--- proyectos/ejerciciosif/2if.py
+++ proyectos/ejerciciosif/2if.py
@@ -1,5 +1,15 @@
 print("introduzca un numero de cliente")
 clienteNum = float(input("numero de cliente"))
+
 if clienteNum ==1000:
     print("Ganaste un premio")
+
+else:
+    
+    while clienteNum !=1000:
+        print("No hay premio")
+        print("introduzca un numero de cliente")
+        clienteNum = float(input("numero de cliente"))
+
+    
     
